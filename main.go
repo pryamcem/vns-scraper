4f8e2f29d2a0209ee247f970f38c25622aa190ae
Stanislav Derevianko <pryamcem@gmail.com>
2023-05-03 17:27:50 +0300

    scruper.go: transferred all functions from main.go

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"strings"
+	"time"
 
 	"github.com/go-rod/rod"
 )
@@ -29,7 +28,7 @@ func main() {
 	defer browser.Close()
 	page := browser.MustPage(*link)
 
-	err := Login(page, config)
+	err := login(page, config)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -40,24 +39,30 @@ func main() {
 		log.Fatalf("Login error: %v", err)
 	}
 	defer storage.Close()
-	//storage.ParseToFile(7)
+	//storage.ParseToFile(9)
+	//return
 
 	i := 0
+	trustValue := 5
 	for {
 		page.MustWaitLoad().MustNavigate(*link)
-		button := page.MustWaitLoad().MustElementR("button", "Зробити наступну спробу")
+		//button, err := page.MustWaitLoad().Timeout(time.Second).ElementR("button", "Спроба тесту")
+		//if err != nil {
+		button, err := page.MustWaitLoad().Timeout(time.Second).ElementR("button", "Зробити наступну спробу")
+
+		//}
 		//Timeout(1*time.Second).MustElementR("button", "Спроба тесту").
 		//CancelTimeout().
 		//Timeout(1*time.Second).MustElementR("button", "Продовжуйте свою спробу")
 		button.MustClick()
-		err := makeTest(page, testNum, *storage)
+		err = makeTest(page, testNum, *storage)
 		if err != nil {
 			log.Fatalln("Test answering error:", err)
 		}
 		finishTest(page)
 		if isTestSuccessful(page) {
 			i++
-			if i == 10 {
+			if i == trustValue {
 				break
 			}
 		} else {
@@ -74,86 +79,4 @@ func main() {
 			}
 		}
 	}
-}
-
-func parseAnswers(page *rod.Page) ([]QA, error) {
-	qtexts := page.MustWaitLoad().MustElements(".qtext")
-	for _, qtext := range qtexts {
-		fmt.Println("Question Text:", qtext.MustText())
-	}
-
-	// Find all elements with the class "rightanswer"
-	rightanswers := page.MustElements(".rightanswer")
-	for _, rightanswer := range rightanswers {
-		fmt.Println("Right Answer:", rightanswer.MustText())
-	}
-	var data []QA
-	for i := range qtexts {
-		data = append(data, QA{
-			question:    strings.Join(strings.Fields(qtexts[i].MustText()), " "),
-			rightanswer: rightanswers[i].MustText(),
-		})
-	}
-	return data, nil
-}
-
-// fundTests return list of links with title='Переглянути ваші відповіді в цій спробі'
-func findTests(page *rod.Page) []string {
-	var links []string
-	elements := page.MustWaitLoad().MustElements("a[title='Переглянути ваші відповіді в цій спробі']")
-
-	for _, link := range elements {
-		href := link.MustAttribute("href")
-		links = append(links, *href)
-	}
-	return links
-}
-
-func makeTest(page *rod.Page, testNum int, s Storage) error {
-	page.MustWaitLoad()
-	tests := page.MustElements(".formulation.clearfix")
-
-	// Print the inner text of each element
-	for _, element := range tests {
-		question := element.MustElement(".qtext").MustText()
-		rightanswer, err := s.PickRightanswer(testNum, question)
-		if err != nil {
-			return fmt.Errorf("Can't get rightanswer from storage: %w", err)
-		}
-		answers := element.MustElements(".flex-fill.ml-1")
-		radioBoxes := element.MustElements("input[type='radio']")
-
-		radioBoxes[0].MustClick()
-		for i, a := range answers {
-			if a.MustText() == rightanswer {
-				radioBoxes[i].MustClick()
-				break
-			}
-		}
-	}
-	return nil
-}
-
-func finishTest(page *rod.Page) {
-	button := page.MustElement("input[type='submit'][value='Завершити спробу...']")
-	button.MustClick()
-	button = page.MustWaitLoad().MustElementR("button", "Відправити все та завершити")
-	button.MustClick()
-	modal := page.MustElement(".modal-footer")
-	button = modal.MustElementR("button", "Відправити все та завершити")
-	button.MustClick()
-}
-
-func isTestSuccessful(page *rod.Page) bool {
-	cells := page.MustWaitLoad().MustElements(".cell")
-	for _, cell := range cells {
-		// Get the inner text of the element
-		text := cell.MustText()
-		fmt.Println(text)
-
-		if strings.Contains(text, "(100%)") {
-			return true
-		}
-	}
-	return false
 }
--- scruper.go
+++ scruper.go
@@ -9,8 +9,8 @@ import (
 	"github.com/go-rod/rod"
 )
 
-// Login to VNS by login and password from config.
-func Login(page *rod.Page, data Configuration) error {
+// login to VNS by login and password from config.
+func login(page *rod.Page, data Configuration) error {
 	err := page.WaitLoad()
 	if err != nil {
 		return fmt.Errorf("Can't load page")
@@ -33,6 +33,8 @@ func Login(page *rod.Page, data Configuration) error {
 	return nil
 }
 
+// mustFindTestNum finds test number to store answers to reqared table in databse.
+// It panics if can't find test number in page.
 func mustFindTestNum(page *rod.Page) int {
 	page.MustWaitLoad()
 	// find all <h2> elements on the page and loop through them
@@ -49,4 +51,90 @@ func mustFindTestNum(page *rod.Page) int {
 		}
 	}
 	panic("Can't find test number")
+}
+
+// fundTests return list of links with title='Переглянути ваші відповіді в цій спробі'
+func findTests(page *rod.Page) []string {
+	var links []string
+	elements := page.MustWaitLoad().MustElements("a[title='Переглянути ваші відповіді в цій спробі']")
+
+	for _, link := range elements {
+		href := link.MustAttribute("href")
+		links = append(links, *href)
+	}
+	return links
+}
+
+func makeTest(page *rod.Page, testNum int, s Storage) error {
+	page.MustWaitLoad()
+	tests := page.MustElements(".formulation.clearfix")
+
+	// Print the inner text of each element
+	for _, element := range tests {
+		question := element.MustElement(".qtext").MustText()
+		rightanswer, err := s.PickRightanswer(testNum, question)
+		if err != nil {
+			return fmt.Errorf("Can't get rightanswer from storage: %w", err)
+		}
+		answers := element.MustElements(".flex-fill.ml-1")
+		radioBoxes := element.MustElements("input[type='radio']")
+
+		radioBoxes[0].MustClick()
+		for i, a := range answers {
+			if a.MustText() == rightanswer {
+				radioBoxes[i].MustClick()
+				break
+			}
+		}
+	}
+	return nil
+}
+
+// finishTest finds and clicks all nesesary buttons to complete test.
+func finishTest(page *rod.Page) {
+	button := page.MustElement("input[type='submit'][value='Завершити спробу...']")
+	button.MustClick()
+	button = page.MustWaitLoad().MustElementR("button", "Відправити все та завершити")
+	button.MustClick()
+	modal := page.MustElement(".modal-footer")
+	button = modal.MustElementR("button", "Відправити все та завершити")
+	button.MustClick()
+}
+
+// isTestSuccessful checks rate of correct answers.
+// It returns true if test successful (100% of correct answers)
+func isTestSuccessful(page *rod.Page) bool {
+	cells := page.MustWaitLoad().MustElements(".cell")
+	for _, cell := range cells {
+		// Get the inner text of the element
+		text := cell.MustText()
+		fmt.Println(text)
+
+		if strings.Contains(text, "(100%)") {
+			return true
+		}
+	}
+	return false
+}
+
+// parseAnswers find all open answers of complete test.
+func parseAnswers(page *rod.Page) ([]QA, error) {
+	qtexts := page.MustWaitLoad().MustElements(".qtext")
+	for _, qtext := range qtexts {
+		fmt.Println("Question Text:", qtext.MustText())
+	}
+
+	// Find all elements with the class "rightanswer"
+	rightanswers := page.MustElements(".rightanswer")
+	for _, rightanswer := range rightanswers {
+		fmt.Println("Right Answer:", rightanswer.MustText())
+	}
+	var data []QA
+	for i := range qtexts {
+		data = append(data, QA{
+			question:    strings.Join(strings.Fields(qtexts[i].MustText()), " "),
+			rightanswer: rightanswers[i].MustText(),
+		})
+	}
+	return data, nil
 }
